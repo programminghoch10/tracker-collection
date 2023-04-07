b53038315a3c6b0e31d8dfd42991b5d6bccba415
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2023-04-07 14:14:30 +0200

    switch to envsubstadvanced in lineageos devices tracker

--- lineageos/devices/tracker.sh
+++ lineageos/devices/tracker.sh
@@ -89,40 +89,9 @@ sendDeviceUpdateMessage() {
     # if a device name starts with the manufacturer we omit it
     DEVICENAME=$(echo "$DEVICENAME" | sed "s|^$DEVICEOEM ||g")
 
-    # escape sed replacement strings
-    DEVICECODENAME=$(sed 's/[&/\]/\\&/g' <<< "$DEVICECODENAME")
-    VERSION=$(sed 's/[&/\]/\\&/g' <<< "$VERSION")
-    DOWNLOADURL=$(sed 's/[&/\]/\\&/g' <<< "$DOWNLOADURL")
-    DOWNLOADSHA=$(sed 's/[&/\]/\\&/g' <<< "$DOWNLOADSHA")
-    DEVICEOEM=$(sed 's/[&/\]/\\&/g' <<< "$DEVICEOEM")
-    DEVICENAME=$(sed 's/[&/\]/\\&/g' <<< "$DEVICENAME")
-    ROMTYPE=$(sed 's/[&/\]/\\&/g' <<< "$ROMTYPE")
-    SIZE=$(sed 's/[&/\]/\\&/g' <<< "$SIZE")
-    DATE=$(sed 's/[&/\]/\\&/g' <<< "$DATE")
-    WIKIURL=$(sed 's/[&/\]/\\&/g' <<< "$WIKIURL")
+    MSG="$(envsubstadvanced < message.html)"
+    KEYBOARD="$(envsubstadvanced < message-keyboard.json)"
 
-    MSG=$(cat message.html)
-    MSG=$(sed "s|\$DEVICECODENAME|$DEVICECODENAME|g" <<< "$MSG")
-    MSG=$(sed "s|\$VERSION|$VERSION|g" <<< "$MSG")
-    MSG=$(sed "s|\$DOWNLOADURL|$DOWNLOADURL|g" <<< "$MSG")
-    MSG=$(sed "s|\$DOWNLOADSHA|$DOWNLOADSHA|g" <<< "$MSG")
-    MSG=$(sed "s|\$DEVICEOEM|$DEVICEOEM|g" <<< "$MSG")
-    MSG=$(sed "s|\$DEVICENAME|$DEVICENAME|g" <<< "$MSG")
-    MSG=$(sed "s|\$ROMTYPE|$ROMTYPE|g" <<< "$MSG")
-    MSG=$(sed "s|\$SIZE|$SIZE|g" <<< "$MSG")
-    MSG=$(sed "s|\$DATE|$DATE|g" <<< "$MSG")
-    MSG=$(sed "s|\$WIKIURL|$WIKIURL|g" <<< "$MSG")
-    KEYBOARD=$(cat message-keyboard.json)
-    KEYBOARD=$(sed "s|\$DEVICECODENAME|$DEVICECODENAME|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$VERSION|$VERSION|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$DOWNLOADURL|$DOWNLOADURL|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$DOWNLOADSHA|$DOWNLOADSHA|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$DEVICEOEM|$DEVICEOEM|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$DEVICENAME|$DEVICENAME|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$ROMTYPE|$ROMTYPE|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$SIZE|$SIZE|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$DATE|$DATE|g" <<< "$KEYBOARD")
-    KEYBOARD=$(sed "s|\$WIKIURL|$WIKIURL|g" <<< "$KEYBOARD")
     sendMessage "$MSG" "$KEYBOARD" || return 1
 }
 
